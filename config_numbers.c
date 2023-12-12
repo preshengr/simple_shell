24f58899d65f12f0e641949f8dfe61e7dcb98579
mahi2025 <mahletmasresha08@gamil.com>
2023-12-12 06:53:18 -0800

    config_numbers.c

--- config_numbers.c
+++ config_numbers.c
@@ -1,9 +1,11 @@
 #include "shell.h"
+
 /**
  * _isdigit - check if a character is a number.
  * @c: is the character.
  * Return: 1 digit or 0 otherwise.
  */
+
 int _isdigit(char c)
 {
 	if (c > 47 && c < 58)
