a21f94513fdc01393626e8222b51a9b8f2570567
Precious <preshengr@mail.com>
2023-12-12 13:23:21 +0100

    added the builtin files

---
+++ builtin.c
@@ -0,0 +1,23 @@
+#include "shell.h"
+/**
+ * builtin - indicates the function to perform, according to s.
+ * @s: is the command to compare.
+ * Return: function to perform or NULL.
+ */
+int (*builtin(char *s))(char *, char **, char *, char **, int, int)
+{
+	built ops[] = {
+		{"exit", ex},
+		{"env", envir},
+		{NULL, NULL}
+	};
+	int i = 0;
+
+	while (ops[i].comand)
+	{
+		if (_strcmp(ops[i].comand, s) == 0)
+			return (ops[i].func);
+		i++;
+	}
+	return (NULL);
+}
